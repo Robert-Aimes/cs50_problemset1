f9deb748797abba11e0dd2e55029f4a139d2b643
Robert-Aimes <65087616+Robert-Aimes@users.noreply.github.com>
2024-09-18 16:16:47 -0500

    Create scabble.c

---
+++ problemset2/scabble.c
@@ -0,0 +1,29 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+char get_letter() {
+    char letter;
+    scanf("%c", &letter);
+    return letter;
+}
+
+
+int main(void){
+
+//Create arrays to store the letters and corresponding values
+int values[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+char letters[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 
+                  'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 
+                  'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+
+}
+
+void playGame(){
+
+}
+
+void analyzeScores(player1Word, player2Word) {
+    
+}
